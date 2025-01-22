839f267f7c28aed1b067b8f5fa1d72087b899dce
Ramesh bhandari <rameshbh786@gmail.com>
2025-01-22 22:21:02 +0545

    first commit

---
+++ Movie website/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import Favorites from './pages/Favorites';
+import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+
+function App() {
+
+
+
+  return (
+    //<Home />
+    <div>
+
+      <NavBar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
