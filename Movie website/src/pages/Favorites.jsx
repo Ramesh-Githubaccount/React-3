8f0e2e65e2f0500cd72ae87f2b9371b9e1099f9a
Ramesh bhandari <rameshbh786@gmail.com>
2025-01-22 22:27:31 +0545

    movie component and routing

--- Movie website/src/pages/Favorites.jsx
+++ Movie website/src/pages/Favorites.jsx
@@ -3,12 +3,12 @@
 
 
 function Favorites() {
-    return (<div className="favorites-empty">
-        <h2>No favorites movies yet</h2>
-        <p>Start adding movies to your favorites and they will appear here!</p>
-
-    </div>
-    )
+    return (
+        <div className="favorites-empty">
+            <h2>No favorites movies yet</h2>
+            <p>Start adding movies to your favorites and they will appear here!</p>
+        </div>
+    );
 }
 
 export default Favorites;
